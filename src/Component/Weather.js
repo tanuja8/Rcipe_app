7d3041294b40681edf0738e57fe1165f518f4bb6
Tanuja <tanujakhatun292@gmail.com>
2023-03-01 22:26:46 +0530

    project updated

--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -4,11 +4,13 @@ import "./Weather.css"
 
 const Weather = () => {
   const [city, setCity] = useState("")
+  const [data, setData] = useState("");
   // const [data,setData] = useState("")
   const getData = () => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=67449485f3fad6eb577e6d1ddae5e80a`)
     .then((response)=>{
       console.log(response.data);
+      setData(response.data);
     })
   }
   return (
@@ -17,16 +19,16 @@ const Weather = () => {
         <h1 className='text-center text-white'>WEATHER APP</h1>
         <div className='input_area'>
           <input type="text" className='from-control' value={city} onChange={(event) => {
-            setCity(event.target.value)
+            setCity(event.target.value);
           }} ></input>
           <button type="submit" className="get_btn" onClick={getData}>Get temp</button>
         </div>
         <div className='location_area'>
-          <h1>Kolkata</h1>
+          <h1>{data.name}</h1>
         </div>
         <div>
-          <h3>5.25 deg C</h3>
-          <h4>Min : 5.25 cel | Max 5.25 cel</h4>
+          <h3>{data.main?.temp_max}</h3>
+          <h4>{data.main?.temp_min}</h4>
         </div>
       </div>
 
